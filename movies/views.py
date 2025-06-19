f41389fbe60395a9706cb943c8fa9b6b70a2d2b5
Peter <peters.oluochs@gmail.com>
2025-06-19 21:30:59 +0300

    Created movies page

--- movies/views.py
+++ movies/views.py
@@ -1,3 +1,14 @@
+from tempfile import template
 from django.shortcuts import render
 
-# Create your views here.
+movies = [
+    {'id': 1, 'name': "Titanic", 'description': "Love story", "price": "$12"},
+    {'id': 2, 'name': "Avatar", 'description': "Suspense thriller", "price": "$30"},
+]
+
+def movies_view(request):
+    template_data = {
+        'title': "Movies",
+        'movies': movies   
+    }
+    return render(request, 'movies/movies.html', {'template_data': template_data})
