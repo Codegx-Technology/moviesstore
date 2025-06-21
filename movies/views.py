43b000343894a73bffc812ff2a8fcedd5f447cce
Tiffany <tiffany@Tee.tee.com>
2025-06-21 19:56:28 +0300

    Added grid layout to movies page

--- movies/views.py
+++ movies/views.py
@@ -4,6 +4,14 @@ from django.shortcuts import render
 movies = [
     {'id': 1, 'name': "Titanic", 'description': "Love story", "price": "$12"},
     {'id': 2, 'name': "Avatar", 'description': "Suspense thriller", "price": "$30"},
+    {'id': 3, 'name': "Inception", 'description': "Mind-bending thriller", "price": "$15"},
+    {'id': 4, 'name': "The Godfather", 'description': "Crime drama", "price": "$20"},
+    {'id': 5, 'name': "The Dark Knight", 'description': "Superhero action", "price": "$18"},
+    {'id': 6, 'name': "Pulp Fiction", 'description': "Cult classic", "price": "$10"},
+    {'id': 7, 'name': "Forrest Gump", 'description': "Inspirational drama", "price": "$14"},
+    {'id': 8, 'name': "The Shawshank Redemption", 'description': "Prison drama", "price": "$16"},
+    {'id': 9, 'name': "Schindler's List", 'description': "Historical drama", "price": "$22"},
+    {'id': 10, 'name': "The Matrix", 'description': "Sci-fi action", "price": "$25"}
 ]
 
 def movies_view(request):
