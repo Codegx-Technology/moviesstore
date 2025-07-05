ce553dd98a17cb63b008ab756c4f60ca3411833d
Bony Born <ezekielbon2@gmail.com>
2025-07-05 11:09:37 +0000

    changes for merge

--- moviesstore_main/settings.py
+++ moviesstore_main/settings.py
@@ -126,3 +126,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://localhost:8000',
+    'http://localhost:8000',
+]
